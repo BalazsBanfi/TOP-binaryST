52d7cc1205ce8003b93c61f855113fc08e9cccde
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-23 19:57:19 +0200

    add prettyPrint

--- index.js
+++ index.js
@@ -27,12 +27,25 @@ const tree = (arr) => {
   const myArr = [...new Set(arr.sort((a, b) => a - b))];
   const root = buildTree(myArr);
 
-  console.log(arr);
-  console.log(myArr);
 
   return { root };
 };
 
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
 const myTree = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
+prettyPrint(myTree.root)
 console.log(myTree);
