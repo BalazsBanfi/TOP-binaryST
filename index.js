811267a7df00ff8730867509b0dcb041d81f221a
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-23 20:04:40 +0200

    add insert func

--- index.js
+++ index.js
@@ -27,25 +27,30 @@ const tree = (arr) => {
   const myArr = [...new Set(arr.sort((a, b) => a - b))];
   const root = buildTree(myArr);
 
+  const insert = (data) => {
+    if (root == null) {
+        
+    }
+    
+  };
 
   return { root };
 };
 
-
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
 
+//const myTree = tree([1, 2, 3, 4, 5, 6, 7])
 const myTree = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-prettyPrint(myTree.root)
-console.log(myTree);
+prettyPrint(myTree.root);
