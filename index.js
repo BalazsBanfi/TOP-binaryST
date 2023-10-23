29dc690b140ea2d9be36aaf2cde7653e8711a5f3
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-23 19:54:13 +0200

    add tree function

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const node = (data, left = null, right = null) => {
-    return {
-      data,
-      left,
-      right,
-    };
+  return {
+    data,
+    left,
+    right,
   };
-  
+};
 
+const buildTree = (arr, start = 0, end = arr.length - 1) => {
+  if (start > end) {
+    return null;
+  }
 
-const tree = (arr, start = 0, end = arr.length - 1) => {
-    if (start > end) {
-        return null;
-    }
+  const mid = parseInt((start + end) / 2);
+  const root = node(arr[mid]);
 
-    const mid = parseInt((start + end) / 2);
-    const root = node(arr[mid]);
+  root.left = buildTree(arr, start, mid - 1);
+  root.right = buildTree(arr, mid + 1, end);
 
-    root.left = tree(arr, start, mid - 1);
-    root.right = tree(arr, mid + 1, end);
+  return root;
+};
 
-    return root;
-  };
-  
+const tree = (arr) => {
+  if (!Array.isArray(arr)) {
+    return arr;
+  }
+  const myArr = [...new Set(arr.sort((a, b) => a - b))];
+  const root = buildTree(myArr);
 
+  console.log(arr);
+  console.log(myArr);
 
+  return { root };
+};
 
-const myTree = tree();
+const myTree = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
 console.log(myTree);
-
